73616e512b113534513a60ace297639e935156d1
momoko <momoko.saunders@rigado.com>
2017-03-22 14:32:57 -0700

    First Commit

---
+++ TestCocoaPod/TestClass.swift
@@ -0,0 +1,15 @@
+//
+//  TestClass.swift
+//  TestCocoaPod
+//
+//  Created by Momoko Saunders on 3/22/17.
+//  Copyright © 2017 Bike Seeds. All rights reserved.
+//
+
+import Foundation
+
+open class TestClass {
+    public func getName() -> String {
+        return "momoko"
+    }
+}
---
+++ TestCocoaPod/TestCocoaPod.h
@@ -0,0 +1,19 @@
+//
+//  TestCocoaPod.h
+//  TestCocoaPod
+//
+//  Created by Momoko Saunders on 3/22/17.
+//  Copyright © 2017 Bike Seeds. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TestCocoaPod.
+FOUNDATION_EXPORT double TestCocoaPodVersionNumber;
+
+//! Project version string for TestCocoaPod.
+FOUNDATION_EXPORT const unsigned char TestCocoaPodVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TestCocoaPod/PublicHeader.h>
+
+
